f9714ecb5dab13861ee1df6a407b9835c51da8a6
Isabella <48463632+izzy612@users.noreply.github.com>
2019-11-02 01:02:35 -0400

    Master (#1)
    * Update README.md
    
    * Create CNAME
    
    * Delete CNAME
    
    * minor error

---
+++ build/precache-manifest.7555a0ea982bb96ecfeb0295696f882a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dd31707c037bda6ed76",
+    "url": "/Izzy_T/static/js/runtime~main.7dd31707.js"
+  },
+  {
+    "revision": "633140945e1bbfb3fb9a",
+    "url": "/Izzy_T/static/js/main.63314094.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Izzy_T/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "633140945e1bbfb3fb9a",
+    "url": "/Izzy_T/static/css/main.71700d9c.chunk.css"
+  },
+  {
+    "revision": "1f58e8b4f23f66342756b9c78399cb7f",
+    "url": "/Izzy_T/index.html"
+  }
+];
